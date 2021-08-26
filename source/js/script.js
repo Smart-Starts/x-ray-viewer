0c813c993dba28ffc5395385551ac592055d7623
standupright <55686549+standupright@users.noreply.github.com>
2021-08-26 16:19:13 +0500

    fix chart out

--- source/js/script.js
+++ source/js/script.js
@@ -19,122 +19,12 @@ let detectorsData = {
 
 let labels = [];
 
-const form = document.querySelector('.settings__form');
-const ipForm = document.querySelector('#ip');
-ipForm.placeholder = `Дефолтный айпи: ${defaultUrl}`;
 
-// Валидация формы
-ipForm.addEventListener('invalid', () => {
-  if (ipForm.validity.valueMissing) {
-    ipForm.setCustomValidity('Обязательное поле');
-  } else {
-    ipForm.setCustomValidity('');
-  }
-});
-
-const getIp = (url, newIp) => {
-  try {
-    fetch(url, {
-        newIp,
-      })
-      .then((response) => response.json())
-      .then((newIp) => {
-        // При успешной отправке в инпут вывести сообщение
-        ipForm.value = `Успешно! Новый айпи: ${newIp}`;
-        defaultUrl = newIp;
-      })
-      .catch((error) => console.log(error));
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-// Событие отправки формы
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  getIp(defaultUrl, ipForm.value);
-});
-
-// Get
-const getData = (onSuccess, url) => {
-  try {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        onSuccess(data);
-      })
-      .catch((error) => console.log(error));
-  } catch (error) {
-    console.log(error)
-  }
-};
-
-const inputs = document.querySelectorAll('.detector-input');
-
-const viewData = (data) => {
-  data.data.map((value, index) => {
-    inputs[index].value = value;
-    detectorsData[index+1].push(Number(value));
-    counter = counter + 1;
-    labels.push = counter + 1;
-    myChart.update();
-  })
-}
-
-const resetData = () => {
-  counter = 0; // Сбросить счетчик;
-  labels = [];
-  detectorsData = {
-    1: [],
-    2: [],
-    3: [],
-    4: [],
-    5: [],
-    6: [],
-    7: [],
-    8: [],
-    9: [],
-    10: [],
-  };
-  myChart.update();
-}
-
-let timer = null;
-
-const startTimer = () => {
-  timer = setTimeout(function tick() {
-    resetData();
-    getData(viewData, DATA_URL);
-    timer = setTimeout(tick, TIME_TO_REQUEST);
-  }, TIME_TO_REQUEST);
-};
-
-
-const stopTimer = () => {
-  clearTimeout(timer); // Stop
-}
-
-const buttonGetData = document.querySelector('#button-get-data');
-
-let start = false;
-buttonGetData.addEventListener('click', () => {
-  if (!start) {
-    buttonGetData.classList.add('viewer__get-button--start');
-    startTimer(); // start
-    start = true;
-  } else {
-    buttonGetData.classList.remove('viewer__get-button--start');
-    stopTimer(); // stop
-    start = false;
-  }
-});
 
 //graphics
 const ctx = document.getElementById('myChart');
 const DATA_COUNT = 7;
 const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
-
-
 
 const data = {
   labels: labels,
@@ -203,11 +93,120 @@ const config = {
       },
       title: {
         display: true,
-        text: 'Надеюсь заработает!'
+        text: 'График значений детекторов'
       }
     }
   },
 };
 
-
 var myChart = new Chart(ctx, config);
+
+const form = document.querySelector('.settings__form');
+const ipForm = document.querySelector('#ip');
+ipForm.placeholder = `Дефолтный айпи: ${defaultUrl}`;
+
+// Валидация формы
+ipForm.addEventListener('invalid', () => {
+  if (ipForm.validity.valueMissing) {
+    ipForm.setCustomValidity('Обязательное поле');
+  } else {
+    ipForm.setCustomValidity('');
+  }
+});
+
+const getIp = (url, newIp) => {
+  try {
+    fetch(url, {
+        newIp,
+      })
+      .then((response) => response.json())
+      .then((newIp) => {
+        // При успешной отправке в инпут вывести сообщение
+        ipForm.value = `Успешно! Новый айпи: ${newIp}`;
+        defaultUrl = newIp;
+      })
+      .catch((error) => console.log(error));
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Событие отправки формы
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  getIp(defaultUrl, ipForm.value);
+});
+
+// Get
+const getData = (onSuccess, url) => {
+  try {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        onSuccess(data);
+      })
+      .catch((error) => console.log(error));
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+const inputs = document.querySelectorAll('.detector-input');
+
+const viewData = (data) => {
+  data.data.map((value, index) => {
+    inputs[index].value = value;
+    detectorsData[index+1].push(Number(value));
+    counter = counter + 1;
+    labels.push(counter);
+    myChart.update();
+  })
+}
+
+const resetData = () => {
+  counter = 0; // Сбросить счетчик;
+  labels = [];
+  detectorsData = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+    10: [],
+  };
+  myChart.update();
+}
+
+let timer = null;
+
+const startTimer = () => {
+  timer = setTimeout(function tick() {
+    resetData();
+    getData(viewData, DATA_URL);
+    timer = setTimeout(tick, TIME_TO_REQUEST);
+  }, TIME_TO_REQUEST);
+};
+
+
+const stopTimer = () => {
+  clearTimeout(timer); // Stop
+}
+
+const buttonGetData = document.querySelector('#button-get-data');
+
+let start = false;
+buttonGetData.addEventListener('click', () => {
+  if (!start) {
+    buttonGetData.classList.add('viewer__get-button--start');
+    startTimer(); // start
+    start = true;
+  } else {
+    buttonGetData.classList.remove('viewer__get-button--start');
+    stopTimer(); // stop
+    start = false;
+  }
+});
