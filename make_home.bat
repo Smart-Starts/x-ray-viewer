582ec679fbe744a525e2d378fa77360a08b0d3c2
Sava1984 <zip_s@mail.ru>
2021-09-08 20:54:49 +0500

    Добавлен makefs

---
+++ make_home.bat
@@ -0,0 +1,8 @@
+del fsdata.c
+del fsdata_custom.c
+
+makefsdata.exe -11
+
+ren fsdata.c fsdata_custom.c
+
+copy fsdata_custom.c H:\Work\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http
