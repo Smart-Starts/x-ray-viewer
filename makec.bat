56d37886ed6226646a87c452d3380e234d3b4f72
Sava1984 <zip_s@mail.ru>
2021-09-13 17:34:01 +0500

    Батник для компилинга в контроллер

---
+++ makec.bat
@@ -0,0 +1,9 @@
+makefsdata.exe -11
+
+erase fsdata_custom.c
+
+erase D:\Shabashki\Rentgen\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http\fsdata_custom.c
+
+copy fsdata.c D:\Shabashki\Rentgen\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http\fsdata_custom.c
+
+copy fsdata.c	fsdata_custom.c
