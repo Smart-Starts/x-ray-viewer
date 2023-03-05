7bd4b5bbb0f203c6add74db53a582f09dad50305
Sava1984 <zip_s@mail.ru>
2023-03-05 13:51:45 +0500

    рабортает

--- make_home.bat
+++ make_home.bat
@@ -5,5 +5,5 @@ makefsdata.exe -11
 
 ren fsdata.c fsdata_custom.c
 
-copy fsdata_custom.c H:\Work\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http
+copy fsdata_custom.c H:\Shabashki\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http
 pause
--- makec.bat
+++ makec.bat
@@ -2,8 +2,8 @@ makefsdata.exe -11
 
 erase fsdata_custom.c
 
-erase D:\Shabashki\Rentgen\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http\fsdata_custom.c
+erase D:\Shabashki\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http\fsdata_custom.c
 
-copy fsdata.c D:\Shabashki\Rentgen\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http\fsdata_custom.c
+copy fsdata.c D:\Shabashki\Retgenograf\Rentgenograf\Middlewares\Third_Party\LwIP\src\apps\http\fsdata_custom.c
 
 copy fsdata.c	fsdata_custom.c
